c4f859f448c8fc002cb410e6853f878d72a209cc
Marut <marutpun@gmail.com>
2024-11-08 16:56:09 +0700

    Add Toggle Theme

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,31 +1,42 @@
-const getThemePreference = () => {
-  if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
-    return localStorage.getItem('theme');
-  }
-  return window.matchMedia('(prefers-color-scheme: dark)').matches
-    ? 'dark'
-    : 'light';
-};
-const isDark = getThemePreference() === 'dark';
-document.documentElement.classList[isDark ? 'add' : 'remove']('dark');
-
-if (typeof localStorage !== 'undefined') {
-  const observer = new MutationObserver(() => {
-    const isDark = document.documentElement.classList.contains('dark');
-    localStorage.setItem('theme', isDark ? 'dark' : 'light');
-  });
-  observer.observe(document.documentElement, {
-    attributes: true,
-    attributeFilter: ['class'],
-  });
+const buttonLight = document.querySelector('#buttonLight');
+const buttonSystem = document.querySelector('#buttonSystem');
+const buttonDark = document.querySelector('#buttonDark');
+const themeButtons = [buttonLight, buttonSystem, buttonDark];
+//loader function
+function applySystemTheme() {
+  const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 }
 
-//
+//loader
+window.addEventListener('load', applySystemTheme);
+window
+  .matchMedia('(prefers-color-scheme: dark)')
+  .addEventListener('change', applySystemTheme);
 
-const applyTheme = () => {
-  const isDark = getThemePreference() === 'dark';
-  document.documentElement.classList[isDark ? 'add' : 'remove']('dark');
-};
+themeButtons.forEach(($toggleSelector) => {
+  $toggleSelector.addEventListener('click', function () {
+    const theme = $toggleSelector.getAttribute('data-theme');
 
-// Apply the theme on page load
-applyTheme();
+    themeButtons.forEach((button) =>
+      button.setAttribute('aria-checked', 'false'),
+    );
+
+    if (theme === 'light') {
+      document.documentElement.classList.remove('dark');
+    } else if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else if (theme === 'system') {
+      document.documentElement.classList.toggle(
+        'dark',
+        window.matchMedia('(prefers-color-scheme: dark)').matches,
+      );
+      window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .addEventListener('change', (e) => {
+          document.documentElement.classList.toggle('dark', e.matches);
+        });
+    }
+
+    $toggleSelector.setAttribute('aria-checked', 'true');
+  });
+});
